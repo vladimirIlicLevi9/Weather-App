78991f346f7a95fa6c5008ce8cf96f4ff3eccb2a
Vladimir Ilic <v.ilic@levi9.com>
2020-05-18 22:16:59 +0200

    removed unsubscribe

--- src/app/components/cities/city-weather/city-weather.component.ts
+++ src/app/components/cities/city-weather/city-weather.component.ts
@@ -5,7 +5,6 @@ import { HourlyWeather } from "src/app/models/hourly-weather.model";
 import { Cities } from "src/app/enums/cities.enum";
 import { ToastrService } from "ngx-toastr";
 import { ImageFormat } from "src/app/enums/img-format.enum";
-import { Subscription } from "rxjs";
 
 //Path to city images folder
 const imagePath = "../../../../assets/images/cities/";
@@ -15,7 +14,7 @@ const imagePath = "../../../../assets/images/cities/";
   templateUrl: "./city-weather.component.html",
   styleUrls: ["./city-weather.component.scss"],
 })
-export class CityWeatherComponent implements OnInit, OnDestroy {
+export class CityWeatherComponent implements OnInit {
   @Input()
   cityId: number;
 
@@ -23,9 +22,6 @@ export class CityWeatherComponent implements OnInit, OnDestroy {
   hourlyCityWeather: HourlyWeather[] = [];
   //Hides component from view if an error occurs while getting weather info
   showCityWeather: boolean;
-
-  cityWeatherSubscription$: Subscription;
-  hourlyCityWeatherSubscription$: Subscription;
 
   constructor(
     private cityWeatherService: CityWeatherService,
@@ -38,23 +34,21 @@ export class CityWeatherComponent implements OnInit, OnDestroy {
 
   //Calls cityWeatherServices to get current weather information and store it in cityWeather
   getCityWeather(cityId: number): void {
-    this.cityWeatherSubscription$ = this.cityWeatherService
-      .getCityWeather(cityId)
-      .subscribe(
-        (response) => {
-          this.cityWeather = new CityWeather({ ...response });
-          this.setCiityIcon(this.cityId);
-          this.showCityWeather = true;
-        },
-        (error) => {
-          this.showCityWeather = false;
-        }
-      );
+    this.cityWeatherService.getCityWeather(cityId).subscribe(
+      (response) => {
+        this.cityWeather = new CityWeather({ ...response });
+        this.setCiityIcon(this.cityId);
+        this.showCityWeather = true;
+      },
+      (error) => {
+        this.showCityWeather = false;
+      }
+    );
   }
 
   //Calls cityWeatherService to get hourly weather info and stores it in hourlyCityWeather
   getHourlyCityWeather(): void {
-    this.hourlyCityWeatherSubscription$ = this.cityWeatherService
+    this.cityWeatherService
       .getHourlyCityWeather(
         this.cityWeather.latitude,
         this.cityWeather.longitude
@@ -68,7 +62,6 @@ export class CityWeatherComponent implements OnInit, OnDestroy {
 
   //Hides hourly wether component
   hideHourlyCityWeather(): void {
-    this.hourlyCityWeatherSubscription$.unsubscribe();
     this.hourlyCityWeather = [];
   }
 
@@ -94,10 +87,5 @@ export class CityWeatherComponent implements OnInit, OnDestroy {
         this.cityWeather.cityIcon = `${imagePath}city${ImageFormat.PNG}`;
         break;
     }
-  }
-
-  ngOnDestroy(): void {
-    this.cityWeatherSubscription$.unsubscribe();
-    this.hourlyCityWeatherSubscription$.unsubscribe();
   }
 }
